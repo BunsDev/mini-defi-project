a18e1ca4ea9b130536284737bf2fa01504fbdd48
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2023-10-07 16:08:58 +0200

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,10 +4,15 @@ require('dotenv').config()
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
+  defaultNetwork: "hardhat",
   networks: {
     bscTestnet: {
-      chainId: '97', // bsc testnet
-      url: 'https://data-seed-prebsc-1-s1.bnbchain.org:8545'
+      chainId: 97, // bsc testnet
+      url: 'https://data-seed-prebsc-1-s1.bnbchain.org:8545',
+      accounts: [process.env.PRIVATE_KEY]
     }
+  },
+  etherscan: {
+    apiKey: `${process.env.BSC_API_KEY}`
   }
 };
--- scripts/DeployTreasury.js
+++ scripts/DeployTreasury.js
@@ -1,11 +1,27 @@
 const hre = require('hardhat')
+const { verify } = require("../utils/verify")
 
-export const deployTreasury = async () => {
+const deployTreasury = async () => {
     const accounts = await hre.ethers.getSigners()
     const deployer = accounts[0]
 
-    const SmartRouterAddress = 0x9a489505a00cE272eAa5e07Dba6491314CaE3796; // PancakeSwap Smart Router in BSC Testnet
+    const SmartRouterAddress = "0x9a489505a00cE272eAa5e07Dba6491314CaE3796"; // PancakeSwap Smart Router in BSC Testnet
 
-    const Treasury = (await (await hre.ethers.getContractFactory('Treasury')).connect(deployer).deploy(SmartRouterAddress)).deployed()
-    console.log("Treasury contract address", Treasury.address)
+    const overrides = {
+        gasLimit: 2000000, 
+        gasPrice: ethers.parseUnits('10', 'gwei'),
+    };
+
+    const args = SmartRouterAddress
+    const Treasury = await hre.ethers.getContractFactory('Treasury');
+    const treasury = await Treasury.connect(deployer).deploy(args, overrides);
+    
+    console.log("Treasury contract address:", treasury.address);
+
+    if (process.env.ETHERSCAN_API_KEY) {
+        console.log("Verifying...")
+        await verify(treasury.address, args)
+    }
 }
+
+deployTreasury()
