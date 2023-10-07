f006791f07f409fd0a5c6968c353d5b6da16d593
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2023-10-07 13:36:24 +0200

    add deploy script

--- scripts/DeployTreasury.js
+++ scripts/DeployTreasury.js
@@ -3,4 +3,9 @@ const hre = require('hardhat')
 const deployTreasury = async () => {
     const accounts = await hre.ethers.getSigners()
     const deployer = accounts[0]
+
+    const SmartRouterAddress = 0x9a489505a00cE272eAa5e07Dba6491314CaE3796; // PancakeSwap Smart Router in BSC Testnet
+
+    const Treasury = (await (await hre.ethers.getContractFactory('Treasury')).connect(deployer).deploy(SmartRouterAddress)).deployed()
+    console.log("Treasury contract address", Treasury.address)
 }
